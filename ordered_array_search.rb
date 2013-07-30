ad160bfe9ffb8210e9a3ec36c857064fc329bc2d
Korotaev Danil <greyd@mail333.com>
2013-07-30 21:54:52 +0600

    fix exit case

--- ordered_array_search.rb
+++ ordered_array_search.rb
@@ -6,34 +6,39 @@ N = 100
 
 def search(a, e)
   li, ri = 0, (a.size - 1)
-  real_im = nil
+  real_i = nil
   iterations_count = 0
 
   loop do
     iterations_count += 1
-    im = ((li + ri) / 2).to_i
-    real_im = (im - K) % N
 
-    if e < a[real_im]
-      ri = im
-    elsif e > a[real_im]
-      li = im
+    i = ((li + ri) / 2).to_i
+    real_i = (i - K) % N
+
+    if e < a[real_i]
+      ri = i - 1
+    elsif e > a[real_i]
+      li = i + 1
     else
       break
     end
 
-    break if li == ri
+    if li > ri
+      real_i = nil
+      break
+    end
   end
 
-  puts "Индекс элемента: #{real_im}"
   puts "Количество сравнений: #{Fixnum.comparsions_count}"
   puts "Количество итераций: #{iterations_count}"
+  real_i
 end
 
-a = Array.new(N) {|i| i }.rotate(K)
+a = Array.new(N) {|i| i * 2 }.rotate(K)
 
 puts "Исходный массив: #{a}"
 puts "Введите искомый элемент"
 e = STDIN.gets.to_i
 
-search a, e
+index = search(a, e)
+puts index ? "Индекс элемента: #{index}" : "Элемент не найден"
