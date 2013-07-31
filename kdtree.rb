083c75c01ef7a783b585349f604ec2d904fdf97a
Korotaev Danil <greyd@mail333.com>
2013-07-31 14:17:19 +0600

    100500DTree

---
+++ kdtree.rb
@@ -0,0 +1,46 @@
+class KDTree
+  attr_accessor :left, :right, :data, :field
+
+  def initialize(points, fields, depth = 0)
+    self.field = fields[(depth % fields.size) - 1]
+
+    if points.size == 1
+      self.data = points.first
+      return
+    end
+
+    points.qsort_by!(field)
+    median_index = (points.size / 2).to_i
+
+    self.data = points[median_index]
+
+    self.left  = KDTree.new(points[0..median_index-1], fields, depth + 1) unless median_index < 1
+    self.right = KDTree.new(points[median_index+1..-1], fields, depth + 1) unless median_index+1 >= points.size
+  end
+
+  # Получение пользователей по диапазонам значений полей
+  #
+  # range - Hash диапазонов для каждого поля
+  #
+  # Returns Array
+  def query(range = {})
+    self.class._query(range, self)
+  end
+
+  private
+
+  def self._query(range, node)
+    field = node.field
+    median = node.data.send(field)
+
+    result = []
+    result += _query(range, node.left) if node.left && median >= range[field].first
+    result += _query(range, node.right) if node.right && (median <= range[field].last)
+
+    if range.keys.all? {|field| range[field].include? node.data.send(field) }
+      result << node.data
+    end
+
+    result
+  end
+end
--- users_search.rb
+++ users_search.rb
@@ -1,70 +1,22 @@
 #!/usr/bin/env ruby
+# encoding : utf-8
 require File.join File.dirname(__FILE__), 'extends/array'
-
-class UsersDTree # 2DTree
-  attr_accessor :left, :right, :data, :field
-
-  def initialize(points, depth = 0)
-    self.field = depth.even? ? :age : :weight
-
-    if points.size == 1
-      self.data = points.first
-      return
-    end
-
-    points.qsort_by!(field)
-    median_index = (points.size / 2).to_i
-
-    self.data = points[median_index]
-
-    self.left  = UsersDTree.new(points[0..median_index-1], depth + 1) unless median_index < 1
-    self.right = UsersDTree.new(points[median_index+1..-1], depth + 1) unless median_index+1 >= points.size
-  end
-
-  # Получение пользователей по диапазонам значений полей
-  #
-  # range - Hash диапазоны для каждого из полей
-  #         age - Range
-  #         weight - Range
-  def query(range = {})
-    self.class._query(range, self)
-  end
-
-  private
-
-  def self._query(range, node)
-    field = node.field
-    median = node.data.send(field)
-
-    result = []
-    result += _query(range, node.left) if node.left && median >= range[field].first
-    result += _query(range, node.right) if node.right && (median <= range[field].last)
-
-    if range.keys.all? {|field| range[field].include? node.data.send(field) }
-      result << node.data
-    end
-
-    result
-   end
-end
-
-class User
-  attr_reader :name, :age, :weight
-
-  def initialize(name, age, weight)
-    @name = name
-    @age = age
-    @weight = weight
-  end
-end
+require File.join File.dirname(__FILE__), 'user'
+require File.join File.dirname(__FILE__), 'kdtree'
 
 users = []
-10.times do |age|
-  10.times do |weight|
-    name = age * weight
-    users << User.new(name, age, weight)
+name = 0
+(1..10).each do |age|
+  (1..10).each do |weight|
+    name += age * weight
+    u = User.new(name, age, weight)
+    puts "[#{u.name}, #{u.age}, #{u.weight}]"
+    users << u
   end
 end
 
-users_tree = UsersDTree.new(users)
-p users_tree.query(age: 0..1, weight: 2..3)
+users << User.new('еще один 67', 2, 3)
+children = KDTree.new(users, [:age, :weight]).query age: 1..2,
+                                                    weight: 2..3
+
+puts "Найденны пользователи с именами #{children.map(&:name).join(', ')}"
